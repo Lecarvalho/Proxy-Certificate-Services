2b99f0fc226c36af524e3be8fcbfce7a929ddde7
Leandro Carvalho <cadastro@lecarvalho.net>
2021-09-14 10:17:57 -0400

    first commit

---
+++ certbot/create-dummy-certificate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+rsa_key_size=4096
+data_path="./data/certbot"
+domains=__DOMAIN__
+
+if [ -d "$data_path" ]; then
+  "Existing data found for $domains. Delete de folder and run again "
+  exit
+fi
+
+echo "### Creating dummy certificate for $domains ..."
+path="/etc/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live/$domains"
+docker-compose run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+echo
+
+echo "### Starting nginx ..."
+docker-compose up --force-recreate -d nginx
+echo
+
+echo "### Deleting dummy certificate for $domains ..."
+docker-compose run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$domains && \
+  rm -Rf /etc/letsencrypt/archive/$domains && \
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+echo
