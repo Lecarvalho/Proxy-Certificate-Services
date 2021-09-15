a7a0e5ce2e1b252a241a93b85e38570f3838eebe
Leandro Carvalho <cadastro@lecarvalho.net>
2021-09-15 08:53:05 -0400

    fix scripts

--- certbot/configure-real-certificate-manually.sh
+++ certbot/configure-real-certificate-manually.sh
@@ -1,13 +1,3 @@
-data_path="./data/certbot"
-
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-  echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-  echo
-fi
-
 docker-compose run --rm --entrypoint "\
 certbot certonly --webroot -w /var/www/certbot \
     --email __CERT_EMAIL__ \
